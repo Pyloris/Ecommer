d48a2c9e88af1303a0a5e02801654f5bfa765597
Zahira-Khawaja <102659197+Zahira-Khawaja@users.noreply.github.com>
2023-11-08 18:44:55 +0530

    signup validation and style updated

--- static/js/authenticate.js
+++ static/js/authenticate.js
@@ -1,3 +1,34 @@
+// the below code will check if all the validation if fullfilled.
+const submitBtn = document.querySelector(".submit-btn");
+
+function checkValidation() {
+    const checks = document.querySelectorAll('.icon');
+    const allChecksPassed = Array.from(checks).every((check) => check.classList.contains('tick'));
+
+    if (allChecksPassed) {
+        submitBtn.disabled = false;
+        submitBtn.classList.remove("disabled");
+    } else {
+        submitBtn.disabled = true;
+        submitBtn.classList.add("disabled");
+    }
+}
+
+// password visibility
+function togglePasswordVisibility(inputId) {
+    const passwordInput = document.querySelector(`.${inputId}`);
+    const eyeButton = document.querySelector(".eye-button");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeButton.innerHTML = "🙈"; /* Unicode for open eye icon */
+        
+    } else {
+        passwordInput.type = "password";
+        eyeButton.innerHTML = "👁️";
+    }
+}
+
 // password validation code starts
 const password = document.querySelector('.pwd');
 var inputValue;
@@ -5,52 +36,53 @@ var inputValue;
 password.addEventListener("keyup",(e)=>{
     validatePwd(e.target.value);
     inputValue = e.target.value;
+    checkValidation();
 });
 
 password.addEventListener("focusin",(e)=>{
     document.querySelector('.verify-pwd').classList.add("show");
 });
 
-// password.addEventListener("focusout",(e)=>{
-//     document.querySelector('.verify-pwd').classList.remove("show");
-// });
 
 function validatePwd(value) {
-    const validationRules = [
-        { regex: /[a-z]/, checkboxSelector: ".check3" },
-        { regex: /[A-Z]/, checkboxSelector: ".check2" },
-        { regex: /[0-9]/, checkboxSelector: ".check4" },
-        { regex: /[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]/, checkboxSelector: ".check5" }
-    ];
+    const lower = /[a-z]/g;
+    const upper = /[A-Z]/g;
+    const number = /[0-9]/g;
+    const symbol = /[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]/g;
 
-    validationRules.forEach(rule => {
-        const checkbox = document.querySelector(rule.checkboxSelector);
-        checkbox.checked = value.match(rule.regex) !== null;
-    });
-
-    const check1Checkbox = document.querySelector(".check1");
-    check1Checkbox.checked = value.length >= 8;
+    updateValidationStatus("icon3", value.match(lower));
+    updateValidationStatus("icon2", value.match(upper));
+    updateValidationStatus("icon4", value.match(number));
+    updateValidationStatus("icon5", value.match(symbol));
+    updateValidationStatus("icon1", value.length >= 8);
 }
 
-
+function updateValidationStatus(elementId,isValid) {
+    const element = document.querySelector(`.${elementId}`);
+    // console.log(element);
+    
+    if (isValid) {
+        element.classList.add("tick");
+        element.classList.remove("cross");
+    } else {
+        element.classList.remove("tick");
+        element.classList.add("cross");
+    }
+    
+}
 
 // password match code
 confirmPassword = document.querySelector(".cPwd");
 
 confirmPassword.addEventListener("keyup",(e)=>{
     pwd_matches(e.target.value);
+    checkValidation();
 });
 
 confirmPassword.addEventListener("focusin",(e)=>{
     document.querySelector('.cp').classList.add("show");
 });
 
-
 function pwd_matches(value){
-    if(value === inputValue){
-        document.querySelector(".check6").checked = true;
-    }else{
-        document.querySelector(".check6").checked = false;
-    }
+    updateValidationStatus("icon6", value===inputValue);
 }
-
